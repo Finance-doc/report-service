0a0d4dd539fc010d0377aac88dfcc51b4347d32c
rudalsss <linda284@naver.com>
2025-09-21 11:40:34 +0900

    feat: x-user-email 확인

--- src/main/java/com/financedoc/report_service/controller/ReportController.java
+++ src/main/java/com/financedoc/report_service/controller/ReportController.java
@@ -1,6 +1,7 @@
 package com.financedoc.report_service.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ReportController {
 
     @GetMapping("/test")
-    public String test() {
-        return "Report Service Test OK";
+    public String test(@RequestHeader("X-User-Email") String email) {
+        return "Report Service Test OK & X-User-Email = "+ email;
     }
 }
