8efb8539fe843c2e34b589299044fb899ef1dd52
rudalsss <linda284@naver.com>
2025-09-21 11:29:15 +0900

    init: report-service spring starter project

---
+++ src/main/java/com/financedoc/report_service/controller/ReportController.java
@@ -0,0 +1,15 @@
+package com.financedoc.report_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/report")
+public class ReportController {
+
+    @GetMapping("/test")
+    public String test() {
+        return "Report Service Test OK";
+    }
+}
