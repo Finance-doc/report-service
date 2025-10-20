ca376e68f81b2913adbb2774132eab7696401df2
rudalsss <linda284@naver.com>
2025-10-20 20:21:21 +0900

    refactor: 배포환경 테이블생성을 위한 엔티티스캔범위 조정

--- src/main/java/com/financedoc/report_service/ReportServiceApplication.java
+++ src/main/java/com/financedoc/report_service/ReportServiceApplication.java
@@ -2,8 +2,10 @@ package com.financedoc.report_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.financedoc.report_service.domain")
 public class ReportServiceApplication {
 
 	public static void main(String[] args) {
